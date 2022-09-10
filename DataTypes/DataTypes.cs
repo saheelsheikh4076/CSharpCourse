102deb0614a2296d16639c1a80327d057b4ae2b7
USER <USER@SAHIL>
2022-09-10 19:15:09 +0530

    Add project files.

---
+++ DataTypes/DataTypes.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace DataTypes
+{
+    public class DataTypes
+    {
+        //integer numbers 1, 20, -30
+        byte b = 10; // 0 to 255
+        Byte b1 = 11;
+        sbyte sb = -20; //-128 to 127
+        SByte sb1 = 22;
+
+
+        short s = 0;//-32,768 to 32,767
+        Int16 s1 = 10;//16bit
+
+        int i = 22;//32 bit 
+        Int32 i1 = 22;
+
+        long l = 33; //64bit
+        Int64 l1 = 33;
+
+        //Fractional 1.3, 4.3
+        float f = 1.1f;
+        double d = 1.1d;
+        double d1 = 1.1;
+        decimal dm = 1.1m;
+
+        //Character
+        char c = '$';
+
+        //String (sentences or words)
+        string str = "hello";
+
+
+        //Boolean (True or False)
+        bool b2 = true;
+        Boolean b3 = false;
+    }
+}
---
+++ DataTypes/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+//implicit conversion & explicit conversion
+//implicit (Automatic)
+byte b = 255;
+
+Int16 i = b;
+Int32 i1 = i;
+Int64 i2 = i1;//implicit from lower to higher
+//i = i1;//cannot convert from higher to lower
+
+
+//Explicit Method1
+i1 = (Int32)i2;
+i = (Int16)i1;
+b = (byte)i;
+//Method 2 using Predefined Functions called Methods
+i1 = Convert.ToInt32(i2);
+i = Convert.ToInt16(i1);
+b = Convert.ToByte(i);
+
+//from 1 to 1.0 or 2.1 to 2
+float f = i1;//converted implicitly
+i1 = (int)f;//to be converted explicitly
+i1 = Convert.ToInt32(f);//1.3 ->1 & 1.7 -> 2
+
+char c = '#';//Valued type & all the above are valued type variables
+int i3 = 'a';
+i3 = c;
+i1 = c;
+
+c = (char)i3;
+
+string str = "2";
+int i4 = Convert.ToInt32(str);
+
+str = Convert.ToString(i);
+str = i.ToString();
+str = 3.ToString();
+
+bool bl = Convert.ToBoolean("true");
+bl = Convert.ToBoolean("Truse");
+bl = Convert.ToBoolean("TRUE");
+
+str = bl.ToString();
+str = Convert.ToString(bl);
+//datatype.TryParse out keyword
