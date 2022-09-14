138a02885a1e93c5ec43ccf796e771c4c3306686
saheelsheikh8421 <saheelsheikh8421@gmIL.COM>
2022-09-14 20:07:05 +0530

    Logical Operators

---
+++ DataTypes/LogicalOperators.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    
+    /// <summary>
+    /// Logical Operators
+    /// And (&), Or (|), Not (!)
+    /// </summary>
+    public class LogicalOperators
+    {
+        public void Test()
+        {
+            LogicalOperators operators = new LogicalOperators();
+            int a = 1, b = 1;
+            Console.WriteLine($"And result {operators.And(a, b)}");
+            Console.WriteLine($"Or result {operators.Or(a, b)}");
+            Console.WriteLine($"boolean And result {operators.And(true, false)}");
+            Console.WriteLine($"boolean Or result {operators.Or(true, false)}");
+            Console.WriteLine($"boolean Not result {operators.Not(false)}");
+            Console.WriteLine($"Complx  result {operators.ComplexLogicalComparasion(false, false)}");
+        }
+        public int And(int a, int b)
+        {
+            return a & b;
+        }
+        public int Or(int a, int b)
+        {
+            return a | b;
+        }
+        public bool And(bool a, bool b)
+        {
+            return (a && b);
+        }
+        public bool Or(bool a, bool b)
+        {
+            return (a || b);
+        }
+        public bool Not(bool a)
+        {
+            return !a;
+        }
+        public bool ComplexLogicalComparasion(bool a, bool b)
+        {
+            ///True False
+            ///!(T && F) || !(T || F)
+            ///!(F) || !(T)
+            ///T || F
+            ///T
+            ///False False
+            ///!(F&&F) || !(F||F)
+            ///!(F) || !(F)
+            ///T || T
+            ///T
+
+
+            return !(a && b) || !(a || b);
+        }
+    }
+}
--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -8,36 +8,7 @@ internal class Program
         //or to create a class object
         //or to instantiate a class 
         //or to create a reference variable of a class
-        ArithmaticClass arithmatic = new ArithmaticClass();
-        Console.WriteLine(" --------->result of Addition <--------");
-        int resultAdd = arithmatic.Addition(71, 72);
-        int resultAdd1 = arithmatic.Addition(50, 45, 48);
-
-        Console.WriteLine($"Result of addition is {resultAdd}");//string interpolation
-        Console.WriteLine($"Result of Addition is {resultAdd}");//string interpolation
-        Console.WriteLine($"Result of Addition is {arithmatic.Addition(45, 45, 45, 8)}");
-
-        Console.WriteLine("-------->result of substract<---------");
-        Console.WriteLine($"result of substract is {arithmatic.Substract(386, 243)}");  
-        Console.WriteLine($"result of substract is {arithmatic.Substract(386, 223, 20)}");  
-        Console.WriteLine($"result of substract is {arithmatic.Substract(526, 243, 80, 60 )}");
-
-        Console.WriteLine("-------->result of multiplication<------");
-        Console.WriteLine($"result of Multiply is {arithmatic.Multiply( 11 , 13)}");
-        Console.WriteLine($"result of Multiply is {arithmatic.Multiply( 11 , 13, 1)}");
-        Console.WriteLine($"result of Multiply is {arithmatic.Multiply( 5.5 , 2, 6.5, 2 )}");
-
-        Console.WriteLine("------->result of division<------");
-
-        (int q, int r) = arithmatic.Divide(286 ,2);
-        Console.WriteLine($"result of division-> Remainder is {r} and Quotient is {q}");
-        Console.WriteLine($"Result of Increment is {arithmatic.Increment(12)}");
-        Console.WriteLine($"Result of Decrement is {arithmatic.Decrement(12)}");
-        int a = 12;
-        Console.WriteLine($"Value in a {a}");//12
-        Console.WriteLine($"Value in a {a++}");//12->13
-        Console.WriteLine($"Value in a {++a}");//14
-        Console.WriteLine($"Value in a {--a}");//13
-        Console.WriteLine($"Value in a {a--}");//13
+        AssignmentOperators operators = new AssignmentOperators();
+        Console.WriteLine($"Result {operators.AssignAndShiftLeft(2,3)}");
     }
 }
