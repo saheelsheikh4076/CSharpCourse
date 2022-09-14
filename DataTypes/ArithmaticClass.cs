beace28301e0c9187acc5ba5b81def0c4e223c19
saheelsheikh8421 <saheelsheikh8421@gmIL.COM>
2022-09-14 17:21:40 +0530

    9/14/22 ArithmaticClass

--- DataTypes/ArithmaticClass.cs
+++ DataTypes/ArithmaticClass.cs
@@ -14,15 +14,35 @@
         public int Addition(int a, int b, int c)
         {
             return a + b + c;
+        }public int Addition(int a, int b, int c, int d)
+        {
+            return a + b + c + d;
         }
-        public int Subtract(int a, int b)
+        public int Substract(int a, int b)
         {
             return a - b;
+        }
+        public int Substract(int a, int b, int c)
+        {
+            return a - b - c;
+        }
+        public int Substract(int a, int b , int c, int d)
+        {
+            return a - b - c - d;
         }
         public int Multiply(int a, int b)
         {
             return a * b;
         }
+        public int Multiply(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+        public double Multiply(double a, double b, double c, double d)
+        {
+            return a * b * c * d;
+        }
+        
         public (int Q, int R) Divide(int a, int b)
         {
             int q = a / b;
--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -1,23 +1,43 @@
 ﻿using DataTypes;
-//To create a variabe of class type (OLD fashion)
-//or to create a class instance
-//or to create a class object
-//or to instantiate a class 
-//or to create a reference variable of a class
-ArithmaticClass arithmatic = new ArithmaticClass();
-int resultAdd = arithmatic.Addition(10, 20, 30);
-int resultSubtract = arithmatic.Subtract(10, 20);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //To create a variabe of class type (OLD fashion)
+        //or to create a class instance
+        //or to create a class object
+        //or to instantiate a class 
+        //or to create a reference variable of a class
+        ArithmaticClass arithmatic = new ArithmaticClass();
+        Console.WriteLine(" --------->result of Addition <--------");
+        int resultAdd = arithmatic.Addition(71, 72);
+        int resultAdd1 = arithmatic.Addition(50, 45, 48);
 
-Console.WriteLine($"Result of addition is {resultAdd}");//string interpolation
-Console.WriteLine($"Result of subtraction is {resultSubtract}");//string interpolation
-Console.WriteLine($"Result of multiplication is {arithmatic.Multiply(10,20)}");
-(int q, int r) = arithmatic.Divide(10, 3);
-Console.WriteLine($"result of division-> Remainder is {r} and Quotient is {q}");
-Console.WriteLine($"Result of Increment is {arithmatic.Increment(10)}");
-Console.WriteLine($"Result of Decrement is {arithmatic.Decrement(10)}");
-int a = 10;
-Console.WriteLine($"Value in a {a}");//10
-Console.WriteLine($"Value in a {a++}");//10->11
-Console.WriteLine($"Value in a {++a}");//12
-Console.WriteLine($"Value in a {--a}");//11
-Console.WriteLine($"Value in a {a--}");//11
+        Console.WriteLine($"Result of addition is {resultAdd}");//string interpolation
+        Console.WriteLine($"Result of Addition is {resultAdd}");//string interpolation
+        Console.WriteLine($"Result of Addition is {arithmatic.Addition(45, 45, 45, 8)}");
+
+        Console.WriteLine("-------->result of substract<---------");
+        Console.WriteLine($"result of substract is {arithmatic.Substract(386, 243)}");  
+        Console.WriteLine($"result of substract is {arithmatic.Substract(386, 223, 20)}");  
+        Console.WriteLine($"result of substract is {arithmatic.Substract(526, 243, 80, 60 )}");
+
+        Console.WriteLine("-------->result of multiplication<------");
+        Console.WriteLine($"result of Multiply is {arithmatic.Multiply( 11 , 13)}");
+        Console.WriteLine($"result of Multiply is {arithmatic.Multiply( 11 , 13, 1)}");
+        Console.WriteLine($"result of Multiply is {arithmatic.Multiply( 5.5 , 2, 6.5, 2 )}");
+
+        Console.WriteLine("------->result of division<------");
+
+        (int q, int r) = arithmatic.Divide(286 ,2);
+        Console.WriteLine($"result of division-> Remainder is {r} and Quotient is {q}");
+        Console.WriteLine($"Result of Increment is {arithmatic.Increment(12)}");
+        Console.WriteLine($"Result of Decrement is {arithmatic.Decrement(12)}");
+        int a = 12;
+        Console.WriteLine($"Value in a {a}");//12
+        Console.WriteLine($"Value in a {a++}");//12->13
+        Console.WriteLine($"Value in a {++a}");//14
+        Console.WriteLine($"Value in a {--a}");//13
+        Console.WriteLine($"Value in a {a--}");//13
+    }
+}
