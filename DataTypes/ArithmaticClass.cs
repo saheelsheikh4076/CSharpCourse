e2c2e8d16fb28097381bb7663ec03a35fc3c640a
saheelsheikh8421 <saheelsheikh8421@gmIL.COM>
2022-09-14 20:06:49 +0530

    Arithmatic and Assignemnt

--- DataTypes/ArithmaticClass.cs
+++ DataTypes/ArithmaticClass.cs
@@ -2,6 +2,41 @@
 {
     public class ArithmaticClass
     {
+        public void Test()
+        {
+            ArithmaticClass arithmatic = new ArithmaticClass();
+            Console.WriteLine(" --------->result of Addition <--------");
+            int resultAdd = arithmatic.Addition(71, 72);
+            Addition(71, 72);
+            int resultAdd1 = arithmatic.Addition(50, 45, 48);
+
+            Console.WriteLine($"Result of addition is {resultAdd}");//string interpolation
+            Console.WriteLine($"Result of Addition is {resultAdd}");//string interpolation
+            Console.WriteLine($"Result of Addition is {arithmatic.Addition(45, 45, 45, 8)}");
+
+            Console.WriteLine("-------->result of substract<---------");
+            Console.WriteLine($"result of substract is {arithmatic.Substract(386, 243)}");
+            Console.WriteLine($"result of substract is {arithmatic.Substract(386, 223, 20)}");
+            Console.WriteLine($"result of substract is {arithmatic.Substract(526, 243, 80, 60)}");
+
+            Console.WriteLine("-------->result of multiplication<------");
+            Console.WriteLine($"result of Multiply is {arithmatic.Multiply(11, 13)}");
+            Console.WriteLine($"result of Multiply is {arithmatic.Multiply(11, 13, 1)}");
+            Console.WriteLine($"result of Multiply is {arithmatic.Multiply(5.5, 2, 6.5, 2)}");
+
+            Console.WriteLine("------->result of division<------");
+
+            (int q, int r) = arithmatic.Divide(286, 2);
+            Console.WriteLine($"result of division-> Remainder is {r} and Quotient is {q}");
+            Console.WriteLine($"Result of Increment is {arithmatic.Increment(12)}");
+            Console.WriteLine($"Result of Decrement is {arithmatic.Decrement(12)}");
+            int a = 12;
+            Console.WriteLine($"Value in a {a}");//12
+            Console.WriteLine($"Value in a {a++}");//12->13
+            Console.WriteLine($"Value in a {++a}");//14
+            Console.WriteLine($"Value in a {--a}");//13
+            Console.WriteLine($"Value in a {a--}");//13
+        }
         /// <summary>
         /// Arithmatic Operators
         /// +,-, * , /, %, A++, ++A, A--, --A
---
+++ DataTypes/AssignmentOperators.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    public class AssignmentOperators
+    {
+        public int AssignAsItIs(int a)
+        {
+            int b = a;
+            return b;
+        }
+        public int AssignAndIncrementByOne(int a)
+        {
+            //a = a + 1;
+            a += 1;
+            return a;
+        }
+        public int AssignAndIncrement(int a, int b)
+        {
+            a += b;
+            return a;
+        }
+        public int AssignAndDecrement(int a, int b)
+        {
+            a -= b;
+            return a;
+        }
+        public int AssignAndMultiply(int a, int b)
+        {
+            ///a = a * b;
+            ///
+            a *= b;
+            return a;
+        }
+        public int AssignAndDivideQ(int a, int b)
+        {
+            //a = a/b;
+            a /= b;
+            return a;
+        }
+        public int AssignAndDivideR(int a, int b)
+        {
+            //a = a % b
+            a %= b;
+            return a;
+        }
+        public int AssignAndShiftLeft(int a, int b)
+        {
+            ///2 will be 0010
+            ///if shifted right by 1 it becomes 0001 i.e. 1
+            ///if 2 (0010) is shifted left by 1 then it becomes 0100 i.e. 4
+            ///if 2 (0010) is shifted left by 2 then it becomes 1000 i.e. 8
+            ///
+            a <<= b;
+            return a;
+        }
+        public int AssignAndShiftRight(int a, int b)
+        {
+            a >>= b;
+            return a;
+        }
+    }
+}
