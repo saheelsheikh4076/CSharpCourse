301f8f30a595930dc52587c288520f0c97a4e231
saheelsheikh8421 <saheelsheikh8421@gmIL.COM>
2022-09-14 23:42:04 +0530

    Assignment Operators.cs

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -9,6 +9,14 @@ internal class Program
         //or to instantiate a class 
         //or to create a reference variable of a class
         AssignmentOperators operators = new AssignmentOperators();
-        Console.WriteLine($"Result {operators.AssignAndShiftLeft(2,3)}");
+        Console.WriteLine($"Result of as it is {operators.AssignAsItIs(14)}");
+        Console.WriteLine($"Result of Increment By One {operators.AssignAndIncrementByOne(142)}");
+        Console.WriteLine($"Result of Increment {operators.AssignAndIncrement(140,3)}");
+        Console.WriteLine($"Result of Decrement {operators.AssignAndDecrement(156,13)}");
+        Console.WriteLine($"Result of Multiply {operators.AssignAndMultiply(11,13)}");
+        Console.WriteLine($"Result of Divide {operators.AssignAndDivideQ( 286 , 2)}");
+        Console.WriteLine($"Result of Divide {operators.AssignAndDivideR( 143 , 243)}"); 
+        Console.WriteLine($"Result of shiftLeft {operators.AssignAndShiftLeft(1, 1000)}"); 
+        Console.WriteLine($"Result of ShiftRight {operators.AssignAndShiftRight(1, 0001)}"); 
     }
 }
