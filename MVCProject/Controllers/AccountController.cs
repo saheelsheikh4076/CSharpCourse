47a49560926818fca9dac0c158bbbbc121167e1a
saheelsheikh8421 <saheelsheikh8421@gmIL.COM>
2022-11-04 20:00:44 +0530

    Model Binding

--- MVCProject/Controllers/AccountController.cs
+++ MVCProject/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCProject.ViewModels;
 
 namespace MVCProject.Controllers
 {
@@ -6,7 +7,25 @@ namespace MVCProject.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            //the word 'sentence' in the following instruction is called key and value is written after =
+            ViewData["sentence"] = "Front end of Account Index page coming from backend";
+            //the word 'myname' is key and "Irfan sir" is value
+            ViewBag.myname = "Irfan sir";
+            int i = 10;
+            string s = "abcd";
+            //object o1 = 10;
+            //object o2 = "abcd";
+            //dynamic d1 = "abcd";
+            AccountIndexViewModel model = new AccountIndexViewModel();
+            model.Books = new List<Book>
+            {
+                new Book{Title = ".net", Author = "Microsoft"},
+                new Book{Title = "c#", Author="Irfan sir"},
+                new Book{Title = "computer", Author ="Arshad"},
+                new Book{Title = "Electronics", Author = "Tauhid"}
+            };
+            
+            return View(model);
         }
     }
 }
---
+++ MVCProject/ViewModels/AccountIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MVCProject.ViewModels
+{
+    public class AccountIndexViewModel
+    {
+        public List<Book> Books { get; set; }
+    }
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }       
+    }
+}
