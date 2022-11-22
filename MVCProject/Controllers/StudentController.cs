44399a3d3bc5a325bc96b9cc05af0e7d06c463c4
saheelsheikh8421 <saheelsheikh8421@gmIL.COM>
2022-11-22 20:02:25 +0530

    student index view model

---
+++ MVCProject/Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCProject.ViewModels;
+using System.Data.SqlClient;
+
+namespace MVCProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<StudentIndexViewModel> model = new ();
+            string connectionString = "Data Source=(local);Initial Catalog=TestDb1;Integrated Security=True";
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                
+                using (SqlCommand command = new SqlCommand("select * from StudentTable", connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                model.Add(new StudentIndexViewModel
+                                {
+                                    //Id = reader.GetInt32(0),
+                                    //Id = Convert.ToInt32(reader[0]),
+                                    Id = Convert.ToInt32(reader["Id"]),
+                                    Name = reader["Name"].ToString(),
+                                    Age = Convert.ToInt32(reader["Age"]),
+                                    Gender = Convert.ToInt32(reader["Gender"])
+                                });
+                            }
+                        }
+                    } 
+                }
+                connection.Close();
+            }
+
+            //SqlConnection connection = new SqlConnection(connectionString);
+            //try
+            //{
+            //    connection.Open();
+            //    //
+            //}
+            //catch (Exception)
+            //{
+
+                
+            //}
+            //finally
+            //{//Finally block executes always
+            //    connection.Close();
+            //}
+           
+            return View(model);
+        }
+    }
+}
--- MVCProject/Program.cs
+++ MVCProject/Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();//Roles
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Student}/{action=Index}/{id?}");
 
 app.Run();
