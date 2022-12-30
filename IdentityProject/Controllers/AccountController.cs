c4cdec5bdc167f12c866a588eb783036ec7777ea
saheelsheikh8421 <saheelsheikh8421@gmIL.COM>
2022-12-30 20:05:09 +0530

    Email services

---
+++ IdentityProject/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using IdentityProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = new IdentityUser();
+               // user.Email = model.Email;
+                user.UserName = model.Username;
+                //user.EmailConfirmed = true;
+
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    //Email confirmation token
+                    var token = await userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                    return RedirectToAction("index", "home");
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ IdentityProject/Models/EmailService.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace IdentityProject.Models
+{
+    public class EmailData
+    {
+        public string EmailToName { get; set; }
+        public string EmailToId { get; set; }
+        public string EmailSubject { get; set; }
+        public string EmailBody { get; set; }
+    }
+    public class EmailService
+    {
+        public bool SendEmail(EmailData emailData)
+        {
+            try
+            {
+                MimeMessage emailMessage = new MimeMessage();
+                MailboxAddress emailFrom = new MailboxAddress("Admin", "admin@abcd.com");
+                emailMessage.From.Add(emailFrom);
+                MailboxAddress emailTo = new MailboxAddress(emailData.EmailToName, emailData.EmailToId);
+                emailMessage.To.Add(emailTo);
+                emailMessage.Subject = emailData.EmailSubject;
+                BodyBuilder emailBodyBuilder = new BodyBuilder();
+                emailBodyBuilder.TextBody = emailData.EmailBody;
+                emailMessage.Body = emailBodyBuilder.ToMessageBody();
+                SmtpClient emailClient = new SmtpClient();
+                emailClient.Connect("servername", 547,false);
+                emailClient.Authenticate("username", "password");
+                emailClient.Send(emailMessage);
+                emailClient.Disconnect(true);
+                emailClient.Dispose();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //Log Exception Details
+                return false;
+            }
+        }
+    }
+}
