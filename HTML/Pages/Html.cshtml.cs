94432a30b59ef36d842e6644b2cdf8f3f0ed47e1
saheelsheikh8421 <saheelsheikh8421@gmIL.COM>
2023-01-30 20:01:03 +0530

    java

---
+++ HTML/Pages/Html.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HTML.Pages
+{
+    public class HtmlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ HTML/Pages/Java.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HTML.Pages
+{
+    public class JavaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
