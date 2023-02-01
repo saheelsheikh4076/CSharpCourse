ad99308334d672b51ffe1858324c117649cb5be5
saheelsheikh8421 <saheelsheikh8421@gmIL.COM>
2023-02-01 20:05:12 +0530

    api

--- MVCProject/Controllers/StudentController.cs
+++ MVCProject/Controllers/StudentController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MVCProject.Models;
 using MVCProject.ViewModels;
 using Services;
 using System.Data.SqlClient;
@@ -26,6 +28,31 @@ namespace MVCProject.Controllers
             var model = await student.GetAllStudents();
             return View(model);
         }
+        [HttpGet]
+        public async Task<IActionResult> GetStudentsApi()
+        {
+            try
+            {
+                List<StudentViewModel> model = await student.GetAllStudents();
+                return Json(new ApiResponse<List<StudentViewModel>>
+                {
+                    IsSuccess = true,
+                    Message = "",
+                    Data = model
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new ApiResponse<List<StudentViewModel>>
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                    Data = new List<StudentViewModel>()
+                });
+            }
+            
+        }
+
 
         [HttpGet]
         public IActionResult AddStudent()
@@ -46,18 +73,111 @@ namespace MVCProject.Controllers
             return View(model);
         }
         [HttpPost]
+       // [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddStudentApi(StudentViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await student.AddStudent(model);
+                    return Json(new ApiResponse<string>
+                    {
+                        IsSuccess = true,
+                        Message = "Student added successfully",
+                        Data = ""
+                    });
+                }
+                catch (Exception ex)
+                {
+                    return Json(new ApiResponse<string>
+                    {
+                        IsSuccess = false,
+                        Message = ex.Message,
+                        Data = ""
+                    });
+                }
+            }
+            else
+            {
+                var errorList = ModelState.Select(x => x.Value.Errors)
+                           .Where(y => y.Count > 0)
+                           .ToList();
+                var errors = string.Empty;
+                foreach (var error in errorList)
+                {
+                    errors += error.ToString() + ",";
+                }
+                return Json(new ApiResponse<string>
+                {
+                    IsSuccess = false,
+                    Message = errors,
+                    Data = ""
+                });
+            }
+        }
+        [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteStudent(string StudentId)
         {
             await student.DeleteStudent(StudentId);
             return RedirectToAction("Students");
         }
+        [HttpPost]
+       // [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteStudentApi(string StudentId)
+        {
+            try
+            {
+                await student.DeleteStudent(StudentId);
+                return Json(new ApiResponse<string>
+                {
+                    IsSuccess = true,
+                    Message = "Student deleted successfully",
+                    Data = ""
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new ApiResponse<string>
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                    Data = ""
+                });
+            }
+            
+        }
 
         [HttpGet]
         public async Task<IActionResult> UpdateStudent(string StudentId)
         {
-            var model = await student.GetStudentById(StudentId);
+            StudentViewModel model = await student.GetStudentById(StudentId);
             return View(model);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetStudentByIdApi(string StudentId)
+        {
+            try
+            {
+                StudentViewModel model = await student.GetStudentById(StudentId);
+                return Json(new ApiResponse<StudentViewModel>
+                {
+                    IsSuccess = true,
+                    Message = "",
+                    Data = model
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new ApiResponse<string>
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                    Data = ""
+                });
+            }
+           
         }
 
         [HttpPost]
@@ -70,6 +190,48 @@ namespace MVCProject.Controllers
                 return RedirectToAction("Students");
             }
             return View(model);
+        }
+
+        [HttpPost]
+       // [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateStudentApi(StudentViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await student.UpdateStudent(model);
+                    return Json(new ApiResponse<string>
+                    {
+                        IsSuccess = true,
+                        Message = "Record updated successfully",
+                        Data = ""
+                    });
+                }
+                catch (Exception ex)
+                {
+                    return Json(new ApiResponse<string>
+                    {
+                        IsSuccess = false,
+                        Message = ex.Message,
+                        Data = ""
+                    });
+                }                
+            }
+            var errorList = ModelState.Select(s => s.Value.Errors)
+                            .Where(s => s.Any())
+                            .ToList();
+            var errors = string.Empty;
+            foreach (var error in errorList)
+            {
+                errors += error.ToString() + ",";
+            }
+            return Json(new ApiResponse<string>
+            {
+                IsSuccess = false,
+                Message = errors,
+                Data = ""
+            });
         }
 
         public IActionResult Index()
---
+++ MVCProject/Models/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCProject.Models
+{
+    public class ApiResponse<T>
+    {
+        public string Message { get; set; }
+        public bool IsSuccess { get; set; }
+        public T Data { get; set; }
+    }
+}
